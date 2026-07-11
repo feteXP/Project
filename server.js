3ff00a452d351249bbcd1731363477dd468dd643
xTymezzAlt <fete3712@gmail.com>
2026-07-11 11:08:35 +0200

    main

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/fetch", async (req,res) => {
+    const uri = req.query.url;
+
+    if (!uri) {
+        return res.status(400).json({error: "Missing URL query. Please add the URL to the query."})
+    }
+    
+    try {
+        const response = await fetch(url);
+        const data = response.text;
+        res.send(data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+})
+
+app.listen(3000);
