30d52187fdb1a0eab65672fb0f35a563397bb642
xTymezzAlt <fete3712@gmail.com>
2026-07-11 18:27:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,6 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-app.get("/fetch", async (req,res) => {
-    const uri = req.query.url;
-
-    if (!uri) {
-        return res.status(400).json({error: "Missing URL query. Please add the URL to the query."})
-    }
-    
-    try {
-        const response = await fetch(uri);
-        const data = await response.text();
-        res.send(data);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-})
-
 const limiter = rateLimit({
     windowMs: 10 * 1000,
     max: 5,
@@ -27,10 +11,31 @@ const limiter = rateLimit({
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("Listening!")
-});
-
 app.use("/fetch", limiter);
 
+app.get("/fetch", async (req, res) => {
+    const uri = req.query.url;
+
+    if (!uri) {
+        return res.status(400).json({
+            error: "Missing URL query. Please add the URL to the query."
+        });
+    }
+
+    try {
+        const response = await fetch(uri);
+        const data = await response.text();
+
+        res.send(data);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Listening!");
+});
