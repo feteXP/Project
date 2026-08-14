8c1fdb648d65fb8162c45c40cc69e77a140e2212
xTymezzAlt <fete3712@gmail.com>
2026-08-14 18:34:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,26 @@ app.get("/fetch", async (req, res) => {
         });
     }
 });
+app.post("/fetch", async (req, res) => {
+    const uri = req.query.url;
+
+    if (!uri) {
+        return res.status(400).json({
+            error: "Missing URL query. Please add the URL to the query."
+        });
+    }
+
+    try {
+        const response = await fetch(uri);
+        const data = await response.text();
+
+        res.send(data);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
